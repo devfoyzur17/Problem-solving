c6b7677d71d0908f78fa54f7746cec29fd03f462
foyzur17 <foyzur17@gmail.com>
2022-06-19 23:42:47 +0600

    5 problem solve

---
+++ 3area_of_circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+
+    double A = 3.14159, result, R;
+    scanf("%lf",&R);
+    result = A*(R*R);
+    printf("A=%.4lf\n",result);
+    return 0;
+}
---
+++ average1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+    double A,B,MEDIA;
+        scanf("%lf",&A),
+        scanf("%lf",&B),
+
+
+        printf("MEDIA = %.5lf",(A*3.5 + B*7.5)/(3.5+7.5));
+        return 0;
+}
